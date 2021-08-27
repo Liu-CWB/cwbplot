393097b890037a6625a36d3204afeff00d52ad98
Liu-CWB <littlepon4@hotmail.com>
2021-08-27 23:26:51 +0800

    conf.py change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,12 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('.'))
 
-
-# -- Project information -----------------------------------------------------
 
 project = 'cwbplot'
 copyright = '2021, CCLiu'
@@ -48,7 +47,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
