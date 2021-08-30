412ea1fd7f414d178f0105b4fe1464576a7ceba0
Liu-CWB <34766410+Liu-CWB@users.noreply.github.com>
2021-08-30 10:45:48 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,8 @@ html_theme = "sphinx_rtd_theme"
 #html_theme = "sphinx_book_theme"
 #html_title = "My site title"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = './images/cwbrfs-logos_transparent.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
