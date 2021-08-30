408afaf75d9c2d9be8c68f89717ecf3d5778a7b3
Liu-CWB <34766410+Liu-CWB@users.noreply.github.com>
2021-08-30 11:09:48 +0800

    add-logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ html_theme = "sphinx_rtd_theme"
 #html_theme = "sphinx_book_theme"
 #html_title = "My site title"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = './image/cwbrfs-logos_transparent.png'
+html_logo = './image/cwbrfs-logos_transparent-cut.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
