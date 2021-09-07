27ca755baa423df889944f255b9ef751b2cdfdac
Liu-CWB <34766410+Liu-CWB@users.noreply.github.com>
2021-09-07 15:03:58 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,7 @@ pygments_style = 'material'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 
-  'sphinx_panels'
-]
+extensions = [ ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
